09e652d79c66beacd8bdb84916a7fc9e84c6df3d
Chewbacca <scu.imaginarium@gmail.com>
2018-11-01 19:59:31 -0700

    initial upload

---
+++ Assets/RecordStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/audioEvent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioEvent : MonoBehaviour {
+    public AudioClip ella;
+    public AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void Play()
+    {
+        audioSource.PlayOneShot(ella);
+    }
+}
